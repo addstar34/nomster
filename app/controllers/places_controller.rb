47502200497167a9f04b1c31ebeda17c62ac6e12
Adam D <addstar34@gmail.com>
2015-08-21 04:59:29 +0000

    new page for creating new places and install of simple_form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,10 @@
 class PlacesController < ApplicationController
-	def index
-	  @places = Place.all.page(params[:page]).per(1)
-	end
+  def index
+	@places = Place.all.page(params[:page]).per(1)
+  end
+
+  def new
+	@place = Place.new
+  end
+
 end
