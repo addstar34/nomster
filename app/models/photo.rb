5610a23de40c566ee42cf2ec7bab01e501b17e4c
Adam D <addstar34@gmail.com>
2015-09-20 12:08:25 +0000

    set file size restriction for picture uploads via the photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,4 +3,13 @@ class Photo < ActiveRecord::Base
 	belongs_to :place
 
 	mount_uploader :picture, PictureUploader
+
+	validate :picture_size_validation
+
+	private
+
+	def picture_size_validation
+		errors[:picture] << "Must be less than 1MB" if picture.size > 1.megabytes
+	end
+
 end
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -33,8 +33,11 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-     process :resize_to_fit => [200, 200]
+     #process :resize_to_fill => [200, 200]
+    process :resize_and_crop => 200
   end
+
+
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
@@ -47,5 +50,20 @@ class PictureUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+  private
+  
+  def resize_and_crop(size)  
+      manipulate! do |image|                 
+        if image[:width] < image[:height]
+          remove = ((image[:height] - image[:width])/2).round 
+          image.shave("0x#{remove}") 
+        elsif image[:width] > image[:height] 
+          remove = ((image[:width] - image[:height])/2).round
+          image.shave("#{remove}x0")
+        end
+        image.resize("#{size}x#{size}")
+        image
+      end
+    end
 
 end
