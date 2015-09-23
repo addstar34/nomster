b946304cd636abd082f8218a67f038f5dc1aac7b
Adam D <addstar34@gmail.com>
2015-09-23 13:14:52 +0000

    adding mailgun config in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,15 @@ Nomster::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.action_mailer.default_url_options = { :host => 'nomster-addstar34.herokuapp.com' }
+
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
 end
